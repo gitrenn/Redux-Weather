c6579bfd38da17c45af14a37f7c38eab4f6c64e9
Ren <rjingmembers@gmail.com>
2018-02-26 15:55:50 -0800

    obtained weather data using open weather API via axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = 'e1ea0831d29d1d730b4e288865a79f73';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+export function fetchWeather(city) { // action is an object that always has a type
+    const url = `${ROOT_URL}&q=${city},us&appid=${API_KEY}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
 import { Form, Input, Icon, Button } from 'semantic-ui-react';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = { term: ''};
@@ -13,9 +17,10 @@ export default class SearchBar extends Component {
     }
 
     handleSubmit(event) {
-
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: ''});
     }
-    
+
     render() {
         return (
             <div>
@@ -26,7 +31,7 @@ export default class SearchBar extends Component {
                         icon='sun'
                         placeholder='Get a five day forecast in yoru favorite cities...' />
                     <Button
-                        onClick={(event) => this.handleSubmit(event)}
+                        onClick={event => this.handleSubmit(event)}
                         primary
                         type='submit'
                     >
@@ -37,3 +42,9 @@ export default class SearchBar extends Component {
         )
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);// passing null because this component doesn't care about state, instead it's passing a function
