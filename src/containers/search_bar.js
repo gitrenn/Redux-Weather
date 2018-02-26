436c068185bfb1457af6716ac5ea42a5ffbe180b
Ren <rjingmembers@gmail.com>
2018-02-26 10:43:45 -0800

    added event handlers on search_bar container

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import { Form, Input, Icon, Button } from 'semantic-ui-react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: ''};
+        this.onInputChange = this.onInputChange.bind(this);// new practice is to use fat arrow function in onChange{(event) => this.onInputChange(event)} if babel is included
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value});
+    }
+
+    handleSubmit(event) {
+
+    }
+    
     render() {
         return (
             <div>
                 <Form>
                     <Input
+                        value={this.state.term}
+                        onChange={this.onInputChange}
                         icon='sun'
-                        placeholder='Search the city...' />
+                        placeholder='Get a five day forecast in yoru favorite cities...' />
                     <Button
+                        onClick={(event) => this.handleSubmit(event)}
+                        primary
                         type='submit'
                     >
                         Search    
